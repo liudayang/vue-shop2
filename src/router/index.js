1dea6875788a16b0b71c11b32dc36eaeb35cd537
Your Name <you@example.com>
2017-11-18 15:40:55 +0800

    HTML 转vue组件

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import GoodList from '@/views/GoodList'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/HelloWorld',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/GoodList',
+      name: 'GoodList',
+      component: GoodList
     }
   ]
 })
